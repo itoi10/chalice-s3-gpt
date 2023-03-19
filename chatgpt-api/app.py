9baa28486b5a63f8be0dff20d4b2df51b4938584
itict10 <itict10@gmail.com>
2023-03-19 20:01:01 +0900

    add longtext endpoint

--- chatgpt-api/app.py
+++ chatgpt-api/app.py
@@ -1,29 +1,36 @@
-from chalice import Chalice
+import openai
+from chalice import Chalice, CORSConfig
 
-app = Chalice(app_name='chatgpt-api')
-
-
-@app.route('/')
-def index():
-    return {'hello': 'world'}
+app = Chalice(app_name="chatgpt-api")
+cors_config = CORSConfig(allow_origin="*")
 
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+@app.route("/longtext", methods=["GET", "POST"], cors=cors_config)
+def longtext():
+    try:
+        request = app.current_request
+
+        if request.method == "GET":
+            text = request.query_params["text"]
+
+        elif request.method == "POST":
+            text = request.json_body["text"]
+
+        messages = [
+            {"role": "system", "content": "あなたは長文生成ツールです。「」の中のテキストを無駄に長い文章に変換してください。"},
+            {"role": "user", "content": f"「{text}」"},
+        ]
+
+        # ChatGPTに送信
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",  # https://platform.openai.com/docs/models/gpt-3-5
+            messages=messages,
+            temperature=1,  # 0に近いほど決まった文章になる 0~2 デフォルト1
+        )
+        # レスポンスからメッセージを取得
+        ai_message = response["choices"][0]["message"]["content"]
+        # メッセージ一覧を返す
+        return {"result": ai_message}
+
+    except Exception as e:
+        return {"error": f"{e}"}
